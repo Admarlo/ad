124f38ff989ac8ba492985a71ce8b1b8e5233577
Admarlo <2dam.adrlop@institutoserpis.org>
2017-11-20 13:05:31 +0100

    Añadido comboBox

--- CComboBox/CComboBox/ComboBoxHelper.cs
+++ CComboBox/CComboBox/ComboBoxHelper.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-namespace CComboBox
+using Gtk;
+using System.Data;
+
+namespace Serpis.Ad
 {
     public class ComboBoxHelper
     {
-        public ComboBoxHelper()
-        {
+        public const string NullLabel ="< sin asignar >"; //internalización y traducción
+        public static void Fill (ComboBox comboBox, string selectSql, object id){
+            id = id.ToString();
+            CellRendererText cellRendererText = new CellRendererText();
+            comboBox.PackStart(cellRendererText, false);
+            comboBox.AddAttribute(cellRendererText,"text 1",1);
+
+            IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+            dbCommand.CommandText = selectSql;
+            IDataReader dataReader = dbCommand.ExecuteReader();
+
+            ListStore listStore = new ListStore(typeof(string), typeof(string));//creamos liststore con 2 columnas
+            comboBox.Model = listStore;
+            TreeIter initialTreeIter = listStore.AppendValues("0", NullLabel);
+            while(dataReader.Read()){//leemos dataReader
+                TreeIter treeIter = listStore.AppendValues(dataReader[0].ToString(), dataReader[1].ToString());
+                if (id.Equals(dataReader[0]))//lo comparamos con la primera posición de id 
+                    initialTreeIter = treeIter;//se lo asignamos a initialTreeIter
+            }
+            dataReader.Close();
+            comboBox.SetActiveIter(initialTreeIter);//se lo pasamos al comboBox
+
+            //TODO obtener el id del comboBox (similar a treeVierHelper de CSerpisAd
         }
     }
 }
--- CComboBox/CComboBox/MainWindow.cs
+++ CComboBox/CComboBox/MainWindow.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Gtk;
+using MySql.Data.MySqlClient;
+
+using Serpis.Ad;
 
 public partial class MainWindow : Gtk.Window
 {
@@ -11,18 +14,24 @@ public partial class MainWindow : Gtk.Window
         comboBox.PackStart(idCellRendererText, false);//para crear columnas
         comboBox.AddAttribute(idCellRendererText,"text",0);*///crea la primera columna
 
-        CellRendererText labelCellRendererText = new CellRendererText();//para hacer la segunda columna
+        /*CellRendererText labelCellRendererText = new CellRendererText();//para hacer la segunda columna (renderizador)
         comboBox.PackStart(labelCellRendererText, false);
         comboBox.AddAttribute(labelCellRendererText,"text",1);
 
         ListStore listStore = new ListStore(typeof(string));
         comboBox.Model = listStore;
-        TreeIter treeIter = listStore.AppendValues("0", "<sin asignar>");
+        TreeIter treeIter = listStore.AppendValues("0", "<sin asignar>");//Valor que se visualizara inicialmente
         listStore.AppendValues("1","cat 1");
         listStore.AppendValues("2", "cat 2");
         listStore.AppendValues("3", "cat 3");
 
-        comboBox.SetActiveIter(treeIter);//Se posiciona en el valor seleccionado linea 20
+        comboBox.SetActiveIter(treeIter);*///Se posiciona en el valor seleccionado linea 20
+
+
+        App.Instance.Connection = new MySqlConnection("server=localhost;database=dbprueba;user=root;password=sistemas");
+        App.Instance.Connection.Open();
+        ComboBoxHelper.Fill(comboBox, "select id, nombre from categoria order by nombre",0);
+
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
