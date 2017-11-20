4bd1502a5d4cb1f0b8cf4199c110e0faf166fbdb
Admarlo <2dam.adrlop@institutoserpis.org>
2017-11-20 13:43:52 +0100

    Añadido comboBox

--- CComboBox/CComboBox/ComboBoxHelper.cs
+++ CComboBox/CComboBox/ComboBoxHelper.cs
@@ -9,17 +9,15 @@ namespace Serpis.Ad
         public const string NullLabel ="< sin asignar >"; //internalización y traducción
         public static void Fill (ComboBox comboBox, string selectSql, object id){
             id = id.ToString();
-            CellRendererText cellRendererText = new CellRendererText();
-            comboBox.PackStart(cellRendererText, false);
-            comboBox.AddAttribute(cellRendererText,"text 1",1);
 
             IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
             dbCommand.CommandText = selectSql;
             IDataReader dataReader = dbCommand.ExecuteReader();
+            init(comboBox);
+            fill(comboBox, dataReader,id);
+            dataReader.Close();
 
-            ListStore listStore = new ListStore(typeof(string), typeof(string));//creamos liststore con 2 columnas
-            comboBox.Model = listStore;
-            TreeIter initialTreeIter = listStore.AppendValues("0", NullLabel);
+           TreeIter initialTreeIter = listStore.AppendValues("0", NullLabel);
             while(dataReader.Read()){//leemos dataReader
                 TreeIter treeIter = listStore.AppendValues(dataReader[0].ToString(), dataReader[1].ToString());
                 if (id.Equals(dataReader[0]))//lo comparamos con la primera posición de id 
@@ -28,7 +26,27 @@ namespace Serpis.Ad
             dataReader.Close();
             comboBox.SetActiveIter(initialTreeIter);//se lo pasamos al comboBox
 
-            //TODO obtener el id del comboBox (similar a treeVierHelper de CSerpisAd
+            //TODO obtener el Get.id del comboBox (similar a treeVierHelper de CSerpisAd
+        }
+
+        private static void init(ComboBox comboBox){
+			CellRendererText cellRendererText = new CellRendererText();
+			comboBox.PackStart(cellRendererText, false);
+			comboBox.AddAttribute(cellRendererText, "text 1", 1);
+			ListStore listStore = new ListStore(typeof(string), typeof(string));//creamos liststore con 2 columnas
+			comboBox.Model = listStore;
+        }
+
+        private static void fill(ComboBox comboBox, IDataReader dataReader,object id){
+            id = id.ToString();
+            ListStore listStore = (ListStore)comboBox.Model;
+            TreeIter initialTreeIter = listStore.AppendValues("0",NullLabel);
+            while(dataReader.Read()){
+                TreeIter treeIter = listStore.AppendValues(dataReader[0].ToString(),dataReader[1].ToString());
+                if (id.Equals(dataReader[0].ToString()))
+                    initialTreeIter = treeIter;
+            }
+            comboBox.SetActiveIter(initialTreeIter);
         }
     }
 }
