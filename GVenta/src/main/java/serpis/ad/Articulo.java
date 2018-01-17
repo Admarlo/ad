f79ae4562cf75804770bfdaa3ec57fad5bf4ea69
Admarlo <2dam.adrlop@institutoserpis.org>
2018-01-17 13:01:49 +0100

    Modificado Pedido y Articulo

--- GVenta/src/main/java/serpis/ad/Articulo.java
+++ GVenta/src/main/java/serpis/ad/Articulo.java
@@ -1,5 +1,60 @@
 package serpis.ad;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Articulo {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	private long id;
+	private String nombre;
+	private BigDecimal precio;
+	@ManyToOne
+	@JoinColumn(name="categoria")
+	private Categoria categoria;
+	
+	public Articulo(long id, String nombre,  BigDecimal precio, Categoria categoria) {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public BigDecimal getPrecio() {
+		return precio;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setId(long id) {
+		this.id=id;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setPrecio(BigDecimal precio) {
+		this.precio = precio;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String toString() {
+		return String.format("[%s] %s %s (%s)", id, nombre,precio,categoria);
+		
+	}
+	
+	
 
 }
--- GVenta/src/main/java/serpis/ad/Cliente.java
+++ GVenta/src/main/java/serpis/ad/Cliente.java
@@ -6,31 +6,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Categoria {
+public class Cliente {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	private String nombre;
 	
+	public long getId() {
+		return id;
+	}
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+	public String getNombre() {
+		return nombre;
+	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	
-	public long getId() {
-		return id;
-	}
-	
-	public String getNombre() {
-		return nombre;
-	}
-	
 	@Override
 	public String toString() {
-		
 		return String.format("[%s] %s", id, nombre);
 }
 
